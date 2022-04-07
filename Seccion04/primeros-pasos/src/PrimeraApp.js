6ba0b876c27e8992a357dd1743d70fa632e18cac
JaimeMatias <matias.raul.jaime@gmail.com>
2022-04-07 19:56:54 -0300

    Seccion04 Creando el componente

--- Seccion04/primeros-pasos/src/PrimeraApp.js
+++ Seccion04/primeros-pasos/src/PrimeraApp.js
@@ -1,4 +1,17 @@
-const  PrimeraApp =()=>{
-return <h1>Hola Mundo</h1>;
-}
+import { Fragment } from "react";
+
+const PrimeraApp = () => {
+
+    //Todas las funciones  pueden devolver 1 unico elemento, para devolver varias etiquetas desde una función
+    //Para poder devolver varias etiquetas, se utiliza el elemento  de abrir y cerrar llave
+    return <>
+        <h1>Hola Mundo</h1>
+        <h1>Mi primera Aplicación</h1>;
+    </>
+
+
+
+
+
+};
 export default PrimeraApp;
