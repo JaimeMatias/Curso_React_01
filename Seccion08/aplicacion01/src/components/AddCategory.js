34f96d268fadf0ae0f6fbda71da85b3bf4115313
JaimeMatias <matias.raul.jaime@gmail.com>
2022-04-20 22:23:22 -0300

    Seccion08 Prueba del Componente AddCategory

--- Seccion08/aplicacion01/src/components/AddCategory.js
+++ Seccion08/aplicacion01/src/components/AddCategory.js
@@ -7,10 +7,12 @@ export const AddCategory = ({ setCategories }) => {
 
     const handleInputChange = ( e ) => {
         setInputValue( e.target.value );
+        console.log('handleInputChange llamado: ',e.target.value)
     }
 
     const handleSubmit = (e) => {
         e.preventDefault();
+        console.log('HandleSubmit: ',inputValue)
 
         if ( inputValue.trim().length > 2 ) {
             setCategories( cats => [ inputValue, ...cats, ] );
@@ -21,6 +23,7 @@ export const AddCategory = ({ setCategories }) => {
 
     return (
         <form onSubmit={ handleSubmit }>
+            <p>{inputValue}</p>
             <input 
                 type="text"
                 value={ inputValue }
