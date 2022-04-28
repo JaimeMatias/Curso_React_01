1fdbce77e79cf76064daa2581759367df5f5c583
JaimeMatias <matias.raul.jaime@gmail.com>
2022-04-27 21:55:43 -0300

    Seccion10 SimpleForm

--- Seccion09/aplicacion01/src/components/02-useEffect/Message.js
+++ Seccion09/aplicacion01/src/components/02-useEffect/Message.js
@@ -1,18 +1,36 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 
 const Message = () => {
-useEffect(()=>{
-    console.log('Componente montado');
-    return ()=>{
-        console.log('Componente Desmontado')
-    }
-},[]  )
+    const [coords, setCoords] = useState({ x: 0, y: 0 })
+    const { x, y } = coords
 
-  return (
-    <div>
-      <h3>Eres Genial</h3>
-    </div>
-  )
+    useEffect(() => {//Cuando el componente se crea se ejecutan estos comandos
+
+        console.log('Componente montado');
+        const mouseMove = (e) => {
+            const coors = { x: e.x, y: e.y }
+            setCoords(coors)
+
+
+        }
+        window.addEventListener('mousemove', mouseMove) //Si yo explicitamente no retiro este evento cuando elimino el componente, esto se continuara ejecutando infinitamente
+
+        return () => {//Cuando el componente deja de existir se ejecutan estos comandos
+            window.removeEventListener('mousemove', mouseMove)
+            console.log('Componente Desmontado')
+        }
+    }, [])
+
+    return (
+        <div>
+            <h3>Eres Genial</h3>
+            <p>
+                X:{x}
+                
+                Y:{y}
+            </p>
+        </div>
+    )
 }
 
 export default Message
