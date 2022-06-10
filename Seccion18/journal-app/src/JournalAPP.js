d011bec96d2ae9da0fc4b8c56b445abf7c666438
JaimeMatias <matias.raul.jaime@gmail.com>
2022-06-09 22:33:11 -0300

    Seccion 18 Journal

---
+++ Seccion18/journal-app/src/JournalAPP.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import AppRouter from './routers/AppRouter'
+
+const JournalAPP = () => {
+  return (
+    <div>
+      <AppRouter/>
+    </div>
+  )
+}
+
+export default JournalAPP
