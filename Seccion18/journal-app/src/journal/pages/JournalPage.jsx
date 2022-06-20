934236ab16450f2a3c090737c448a19a464241b7
JaimeMatias <matias.raul.jaime@gmail.com>
2022-06-20 16:54:36 -0300

    Seccion 18 Finalizacion

--- Seccion18/journal-app/src/journal/pages/JournalPage.jsx
+++ Seccion18/journal-app/src/journal/pages/JournalPage.jsx
@@ -1,4 +1,6 @@
 
+import { AddOutlined } from '@mui/icons-material'
+import { IconButton } from '@mui/material'
 import React from 'react'
 import JournalLayout from '../layout/JournalLayout'
 import NoteView from '../views/NoteView'
@@ -11,7 +13,20 @@ const JournalPage = () => {
         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum
       </Typography> */}
       {/* <NothingSelectedView /> */}
-      <NoteView/>
+      <NoteView />
+      <IconButton size='large'
+        sx={{
+          color: 'white',
+          backgroundColor: 'error.main',
+          ':hover': { backgroundColor: 'error.main', opacity: 0.9 },
+          position: 'fixed',
+          right: 50,
+          bottom: 50
+        }
+        }>
+        <AddOutlined sx={{fontSize:30}}/>
+      </IconButton>
+
     </JournalLayout>
 
   )
